1e35e83ea80f1fb7ec7a69893c88fa6c0b980085
pronobiswas <biswaspronob@outlook.com>
2025-07-28 18:29:37 +0600

    update button as component

--- src/components/architectComponent/ArchitectsBanner.jsx
+++ src/components/architectComponent/ArchitectsBanner.jsx
@@ -1,32 +1,33 @@
 
 import React, { useEffect, useRef } from 'react'
 import { MdArrowOutward } from 'react-icons/md'
+import BtnComponent from '../homeComponents/utils/BtnComponent'
 
 const ArchitectsBanner = () => {
-  let buttonRef = useRef(null);
-  let iconListRef = useRef(null);
-  useEffect(() => {
-    const handleMouseEnter = () => {
-      iconListRef.current.style.transform = ' translate(12px , -12px)';
-      iconListRef.current.style.transition = 'transform 0.3s ease-in-out';
-    };
+  // let buttonRef = useRef(null);
+  // let iconListRef = useRef(null);
+  // useEffect(() => {
+  //   const handleMouseEnter = () => {
+  //     iconListRef.current.style.transform = ' translate(12px , -12px)';
+  //     iconListRef.current.style.transition = 'transform 0.3s ease-in-out';
+  //   };
 
-    const handleMouseLeave = () => {
-      iconListRef.current.style.transform = 'translate(0 , 0)';
-    };
+  //   const handleMouseLeave = () => {
+  //     iconListRef.current.style.transform = 'translate(0 , 0)';
+  //   };
 
-    if (buttonRef.current) {
-      buttonRef.current.addEventListener('mouseenter', handleMouseEnter);
-      buttonRef.current.addEventListener('mouseleave', handleMouseLeave);
-    }
+  //   if (buttonRef.current) {
+  //     buttonRef.current.addEventListener('mouseenter', handleMouseEnter);
+  //     buttonRef.current.addEventListener('mouseleave', handleMouseLeave);
+  //   }
 
-    return () => {
-      if (buttonRef.current) {
-        buttonRef.current.removeEventListener('mouseenter', handleMouseEnter);
-        buttonRef.current.removeEventListener('mouseleave', handleMouseLeave);
-      }
-    };
-  }, []);
+  //   return () => {
+  //     if (buttonRef.current) {
+  //       buttonRef.current.removeEventListener('mouseenter', handleMouseEnter);
+  //       buttonRef.current.removeEventListener('mouseleave', handleMouseLeave);
+  //     }
+  //   };
+  // }, []);
   return (
     <>
       <div className="architectsBanner">
@@ -36,7 +37,7 @@ const ArchitectsBanner = () => {
               <h2 className='text-4xl'>Join the Casewell</h2>
               <h2 className='text-4xl mb-8'>Trade Program</h2>
 
-              <div ref={buttonRef} className='pro_btn w-fit flex items-center gap-2 border py-2 px-5 bg-white text-black cursor-pointer rounded-full'>
+              {/* <div ref={buttonRef} className='pro_btn w-fit flex items-center gap-2 border py-2 px-5 bg-white text-black cursor-pointer rounded-full'>
                 <span>Join now</span>
                 <div className="icon w-[15px] h-[15px] overflow-hidden">
                   <div ref={iconListRef} className='pro_icon_wrapper relative'>
@@ -44,7 +45,10 @@ const ArchitectsBanner = () => {
                     <span className='absolute top-[12px] left-[-12px]'><MdArrowOutward /></span>
                   </div>
                 </div>
-              </div>
+              </div> */}
+              <BtnComponent text="join now" />
+              <BtnComponent text="join tomorrow" />
+              <BtnComponent text="join next day" />
 
             </div>
           </div>
--- src/components/homeComponents/utils/BtnComponent.jsx
+++ src/components/homeComponents/utils/BtnComponent.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { MdArrowOutward } from 'react-icons/md'
 
-const BtnComponent = () => {
+const BtnComponent = ({ text = "" }) => {
+      let buttonRef = useRef(null);
+      let iconListRef = useRef(null);
+      useEffect(() => {
+        const handleMouseEnter = () => {
+          iconListRef.current.style.transform = ' translate(12px , -12px)';
+          iconListRef.current.style.transition = 'transform 0.3s ease-in-out';
+        };
+    
+        const handleMouseLeave = () => {
+          iconListRef.current.style.transform = 'translate(0 , 0)';
+        };
+    
+        if (buttonRef.current) {
+          buttonRef.current.addEventListener('mouseenter', handleMouseEnter);
+          buttonRef.current.addEventListener('mouseleave', handleMouseLeave);
+        }
+    
+        return () => {
+          if (buttonRef.current) {
+            buttonRef.current.removeEventListener('mouseenter', handleMouseEnter);
+            buttonRef.current.removeEventListener('mouseleave', handleMouseLeave);
+          }
+        };
+      }, []);
     return (
         <div>
-            <span>Join now</span>
-            <div className="icon">
-
-                <span><MdArrowOutward /></span>
-                <span><MdArrowOutward /></span>
+            <div ref={buttonRef} className='pro_btn w-fit flex items-center gap-2 border py-2 px-5 bg-white text-black cursor-pointer rounded-full'>
+                <span>{text}</span>
+                <div className="icon w-[15px] h-[15px] overflow-hidden">
+                    <div ref={iconListRef} className='pro_icon_wrapper relative'>
+                        <span><MdArrowOutward /></span>
+                        <span className='absolute top-[12px] left-[-12px]'><MdArrowOutward /></span>
+                    </div>
+                </div>
             </div>
 
         </div>
