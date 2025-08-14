e3a2daf3d342cca6ea185e0528a7d15dcb030e01
pronobiswas <biswaspronob@outlook.com>
2025-08-14 18:20:44 +0600

    update

--- src/components/commonComponent/VideoScroller.jsx
+++ src/components/commonComponent/VideoScroller.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
@@ -6,46 +6,64 @@ gsap.registerPlugin(ScrollTrigger);
 
 const VideoScroller = () => {
     const videoRef = useRef(null);
+    const [videoTime, setVideoTime] = useState(0)
     useEffect(() => {
-    const video = videoRef.current;
+        const video = videoRef.current;
 
-    if (!video) return;
+        if (!video) return;
 
-    const onLoaded = () => {
-      gsap.to(video, {
-        currentTime: video.duration,
-        ease: "none",
-        scrollTrigger: {
-          trigger: video,
-          start: "top center",
-          end: "bottom center",
-          scrub: true
-        }
-      });
-    };
+        const onLoaded = () => {
+            setVideoTime(video.duration)
+            gsap.to(video, {
+                currentTime: video.duration,
+                ease: "none",
+                scrollTrigger: {
+                    trigger: videoRef.current,
+                    start: "top top",
+                    end: "+=500",
+                    pin:true,
+                    scrub: true,
+                    markers:true
+                }
+            });
+        };
 
-    video.addEventListener("loadedmetadata", onLoaded);
+        video.addEventListener("loadedmetadata", onLoaded);
+        
 
-    return () => {
-      video.removeEventListener("loadedmetadata", onLoaded);
-      ScrollTrigger.getAll().forEach(trigger => trigger.kill());
-    };
-  }, []);
+        return () => {
+            video.removeEventListener("loadedmetadata", onLoaded);
+            ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+        };
+    }, []);
 
     return (
         <>
+        <section id='banner'>
+            <div className="wrapper w-full h-screen">
+                <div className="rows w-full h-1/4 border-b"></div>
+                <div className="rows w-full h-1/4 border-b"></div>
+                <div className="rows w-full h-1/4 border-b"></div>
+                <div className="rows w-full h-1/4 border-b"></div>
+
+            </div>
+        </section>
             <section id="videoScroler">
+                <div className="w-full h-"></div>
                 <div className="wrapper w-full h-screen bg-colorOne flex justify-center items-center">
-                    <video
-                        ref={videoRef}
-                        preload="auto"
-                        muted
-                        playsInline
-                        style={{ maxWidth: "100%", height: "auto", display: "block" }}
-                    >
-                        <source src="/video/video1.mp4" type="video/mp4" />
-                        Your browser does not support the video tag.
-                    </video>
+                    <div className='w-full h-full'>
+
+                        <video
+                            ref={videoRef}
+                            preload="auto"
+                            muted
+                            playsInline
+                            style={{ maxWidth: "100%", height: "auto", display: "block" }}
+                        >
+                            <source src="/video/video1.mp4" type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
                 </div>
             </section>
         </>
